e134d265ac15e5847e8fff1afbaca5acbdb3ddff
Arnav-eigenstudio <arnav.eigenstudio@gmail.com>
2025-12-12 02:30:12 +0530

    report generated

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+
 // ⚠️ REPLACE with your actual Google Client ID from Cloud Console
 const GOOGLE_CLIENT_ID = "1096730764737-ma3q511kei4goe7kq14on8bk9pj5trtl.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+
             <App />
+
         </GoogleOAuthProvider>
     </React.StrictMode>,
 )
